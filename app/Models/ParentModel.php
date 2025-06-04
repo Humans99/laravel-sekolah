9d7c32687f63f752244b60c733a58f01d918b9ac
Humans99 <samuelsidabalok12@gmail.com>
2025-06-05 06:12:52 +0700

    feat: create Parent model with fillable attributes and relationships to User and Student models

---
+++ app/Models/ParentModel.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ParentModel extends Model
+{
+    protected $table = 'parents';
+
+    protected $fillable = [
+        'user_id',
+        'name',
+        'email',
+        'phone',
+        'address'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function students()
+    {
+        return $this->hasMany(Student::class, 'parent_id');
+    }
+}
