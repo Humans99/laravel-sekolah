a4568da298b52945c334e12653ae1fda23ee5b3e
Humans99 <samuelsidabalok12@gmail.com>
2025-06-05 10:29:26 +0700

    feat: add lessons relationship to Teacher model

--- app/Models/Teacher.php
+++ app/Models/Teacher.php
@@ -20,4 +20,9 @@ class Teacher extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function lessons()
+    {
+        return $this->hasMany((Lesson::class));
+    }
 }
