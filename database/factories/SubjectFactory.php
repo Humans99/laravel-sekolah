91c9e1dcaf4fa843d79a10391b2c3f18204a6569
Humans99 <samuelsidabalok12@gmail.com>
2025-06-05 16:27:44 +0700

    feat: create SubjectFactory for generating subject instances

---
+++ database/factories/SubjectFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Subject;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class SubjectFactory extends Factory
+{
+    protected $model = Subject::class;
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->randomElement(['Matematika', 'IPA', "IPS", "Bahasa Indonesia", 'Bahasa Inggris', 'Seni Budaya', 'PJOK', 'PKN', 'Pendidikan Agama'])
+        ];
+    }
+}
