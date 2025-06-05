2dcd10ce33d2bdfe0720bce4cfa9159965ba6a7b
Humans99 <samuelsidabalok12@gmail.com>
2025-06-05 16:26:58 +0700

    feat: create AttendanceFactory for generating attendance instances

---
+++ database/factories/AttendanceFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Attendance;
+use App\Models\Lesson;
+use App\Models\Student;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AttendanceFactory extends Factory
+{
+    protected $model = Attendance::class;
+    public function definition(): array
+    {
+        return [
+            'student_id' => Student::factory(),
+            'lesson_id' => Lesson::factory(),
+            'date' => $this->faker->date,
+            'status' => $this->faker->randomElement(['Hadir', 'Sakit', 'Ijin', 'Alfa'])
+        ];
+    }
+}
