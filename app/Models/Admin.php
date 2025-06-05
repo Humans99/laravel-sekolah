853f8e587598ed13cc3daa8f2ecd8d5eed756ce9
Humans99 <samuelsidabalok12@gmail.com>
2025-06-05 10:29:18 +0700

    feat: add user relationship to Admin model

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -15,4 +15,9 @@ class Admin extends Authenticatable
         'password',
         'remember_token'
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
