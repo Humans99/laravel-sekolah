b4c3795a572e5bfeceefef2b8a4acd0bf686bc9a
Humans99 <samuelsidabalok12@gmail.com>
2025-06-05 16:28:31 +0700

    feat: remove email_verified_at and remember_token from UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,9 +26,7 @@ class UserFactory extends Factory
         return [
             'username' => fake()->unique()->userName(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
         ];
     }
 
