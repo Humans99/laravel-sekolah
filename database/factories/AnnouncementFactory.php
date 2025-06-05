5f69715ec8e75461d94648dc04d66716d2a7c87b
Humans99 <samuelsidabalok12@gmail.com>
2025-06-05 16:26:47 +0700

    feat: create AnnouncementFactory for generating announcement instances

---
+++ database/factories/AnnouncementFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Announcement;
+use App\Models\ClassModel;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AnnouncementFactory extends Factory
+{
+    protected $model = Announcement::class;
+    public function definition(): array
+    {
+        return [
+            'class_id' => ClassModel::factory(),
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph,
+            'date' => $this->faker->date('Y-m-d', 'now')
+        ];
+    }
+}
