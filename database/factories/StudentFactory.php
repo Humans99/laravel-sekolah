5549ba677a63051596bf28827cd42ca8b033ec22
Humans99 <samuelsidabalok12@gmail.com>
2025-06-06 21:12:27 +0700

    fix: remove username and password fields from StudentFactory

--- database/factories/StudentFactory.php
+++ database/factories/StudentFactory.php
@@ -15,19 +15,15 @@ class StudentFactory extends Factory
     protected $model = Student::class;
     public function definition(): array
     {
-        $username = $this->faker->unique()->userName;
         return [
             'user_id' => User::factory()->create(['role' => 'student'])->id,
             'parent_id' => ParentModel::factory(),
             'grade_id' => Grade::factory(),
             'class_id' => ClassModel::factory(),
-            'username' => $username,
-            'password' => Hash::make($username),
             'name' => $this->faker->name,
-            'email' => $this->faker->unique()->safeEmail(),
             'phone' => $this->faker->unique()->numerify("08##-####-####"),
-            'address' => $this->faker->address,
-            'bloodType' => $this->faker->randomElement(['A', 'AB', 'B', 'O'])
+            'bloodType' => $this->faker->randomElement(['A', 'AB', 'B', 'O']),
+            'bloodType' => $this->faker->randomElement(['Pria', 'Wanita'])
         ];
     }
 }
