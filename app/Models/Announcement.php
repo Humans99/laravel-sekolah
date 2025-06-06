486e553f1e32ebae16438c1ebe814008290eb9a8
Humans99 <samuelsidabalok12@gmail.com>
2025-06-06 10:02:42 +0700

    feat: add HasFactory trait and static factory property to Announcement model

--- app/Models/Announcement.php
+++ app/Models/Announcement.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Announcement extends Model
 {
+    use HasFactory;
+    protected static $factory;
     protected $fillable = ['title', 'description', 'date', 'class_id'];
 
     public function class()
