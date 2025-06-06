f3d302702913bde2fa6e69ba6a8026b291d84383
Humans99 <samuelsidabalok12@gmail.com>
2025-06-06 20:30:14 +0700

    fix: update ClassSeeder to create specific class names instead of a bulk creation

--- database/seeders/ClassSeeder.php
+++ database/seeders/ClassSeeder.php
@@ -9,6 +9,14 @@ class ClassSeeder extends Seeder
 {
     public function run(): void
     {
-        ClassModel::factory(5)->create();
+        ClassModel::factory()->create(['name' => 'A']);
+        ClassModel::factory()->create(['name' => 'B']);
+        ClassModel::factory()->create(['name' => 'C']);
+        ClassModel::factory()->create(['name' => 'D']);
+        ClassModel::factory()->create(['name' => 'E']);
+        ClassModel::factory()->create(['name' => 'F']);
+        ClassModel::factory()->create(['name' => 'G']);
+        ClassModel::factory()->create(['name' => 'H']);
+        ClassModel::factory()->create(['name' => 'I']);
     }
 }
