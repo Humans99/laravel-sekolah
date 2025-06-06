2f18583e636828454eb4daa96eb7fa07ed8a8227
Humans99 <samuelsidabalok12@gmail.com>
2025-06-06 19:58:58 +0700

    feat: add ParentSeeder for seeding Parent model instances

---
+++ database/seeders/ParentSeeder.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\ParentModel;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ParentSeeder extends Seeder
+{
+    public function run(): void
+    {
+        ParentModel::factory(5)->create();
+    }
+}
