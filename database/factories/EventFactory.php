3bafff5b6ea41cd1f31a7b665a3ca3236178ed21
Humans99 <samuelsidabalok12@gmail.com>
2025-06-06 10:06:42 +0700

    fix: update start and end date generation in EventFactory

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -11,12 +11,13 @@ class EventFactory extends Factory
     protected $model = Event::class;
     public function definition(): array
     {
+        $start = $this->faker->dateTimeBetween('-1 week', '+1 week');
         return [
             'class_id' => ClassModel::factory(),
             'title' => $this->faker->sentence(),
             'description' => $this->faker->paragraph(),
-            'start' => $this->faker->dateTimeBetween('now', '+1 week'),
-            'end' => $this->faker->dateTimeBetween('+1 week', '+2 week'),
+            'start' => $start,
+            'end' => $this->faker->dateTimeBetween($start, '+3 days'),
         ];
     }
 }
