5be709aa849d67ded57ce6a564db1f645c94d5e9
Humans99 <samuelsidabalok12@gmail.com>
2025-06-06 22:33:04 +0700

    fix: add subject_id foreign key to teachers table migration

--- database/migrations/2025_06_05_093923_create_teachers_table.php
+++ database/migrations/2025_06_05_093923_create_teachers_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('teachers', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('subject_id')->constrained()->onDelete('cascade');
             $table->string('name');
             $table->string('phone')->unique();
             $table->string('address');
