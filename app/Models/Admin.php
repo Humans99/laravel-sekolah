724887e604c1063358ad9f84a35c365fa3b1c469
Humans99 <samuelsidabalok12@gmail.com>
2025-06-06 10:02:27 +0700

    feat: add HasFactory trait to Admin model and define static factory property

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class Admin extends Authenticatable
 {
+    use HasFactory;
     use Notifiable;
+    protected static $factory;
 
     protected $fillable = ['user_id', 'username'];
 
