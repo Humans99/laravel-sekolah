6a984f7ad848abafc4e9c2078895fbb2cbc42950
Humans99 <samuelsidabalok12@gmail.com>
2025-06-06 21:47:29 +0700

    fix: update TeacherSeeder to create 31 teacher records

--- database/seeders/TeacherSeeder.php
+++ database/seeders/TeacherSeeder.php
@@ -3,14 +3,12 @@
 namespace Database\Seeders;
 
 use App\Models\Teacher;
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class TeacherSeeder extends Seeder
 {
     public function run(): void
     {
-        Teacher::factory()->create();
+        Teacher::factory(31)->create();
     }
 }
