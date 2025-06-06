041922ce95e18ee3b71cdae5573a795a1c20d072
Humans99 <samuelsidabalok12@gmail.com>
2025-06-06 10:04:40 +0700

    feat: add HasFactory trait and static factory property to ParentModel

--- app/Models/ParentModel.php
+++ app/Models/ParentModel.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ParentModel extends Model
 {
+    use HasFactory;
+    protected static $factory;
     protected $table = 'parents';
 
     protected $fillable = [
