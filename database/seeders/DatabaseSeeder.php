e5fbf892d459f54b031df4630ee2c5f9bf05df4e
Humans99 <samuelsidabalok12@gmail.com>
2025-06-06 20:10:11 +0700

    refactor: remove commented-out user factory code in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,11 +10,22 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            GradeSeeder::class,
+            SubjectSeeder::class,
+            UserSeeder::class,
+            AdminSeeder::class,
+            TeacherSeeder::class,
+            ParentSeeder::class,
+            ClassSeeder::class,
+            StudentSeeder::class,
+            LessonSeeder::class,
+            AssignmentSeeder::class,
+            ExamSeeder::class,
+            AttendanceSeeder::class,
+            AnnouncementSeeder::class,
+            EventSeeder::class,
+            ResultSeeder::class,
         ]);
     }
 }
