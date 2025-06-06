44d248ccb9e30f4c91863a90f5a5f67458f5e405
Humans99 <samuelsidabalok12@gmail.com>
2025-06-06 19:57:13 +0700

    feat: add ClassModelFactory for generating ClassModel instances

---
+++ database/factories/ClassModelFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\ClassModel;
+use App\Models\Teacher;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class ClassModelFactory extends Factory
+{
+    use HasFactory;
+    protected $model = ClassModel::class;
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->unique()->word,
+            'capacity' => $this->faker->numberBetween(20, 40),
+            'supervisor_id' => Teacher::factory()
+        ];
+    }
+}
