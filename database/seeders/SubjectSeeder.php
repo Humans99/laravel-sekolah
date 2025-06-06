7bf51433a70c94c45c92978990868b55fa32b0d5
Humans99 <samuelsidabalok12@gmail.com>
2025-06-06 20:04:04 +0700

    feat: add SubjectSeeder for seeding Subject model instances

---
+++ database/seeders/SubjectSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Subject;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SubjectSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $subjects = ['Matematika', 'IPA', "IPS", "Bahasa Indonesia", 'Bahasa Inggris', 'Seni Budaya', 'PJOK', 'PKN', 'Pendidikan Agama'];
+        foreach ($subjects as $subject) {
+            Subject::factory()->create(['name' => $subject]);
+        }
+    }
+}
