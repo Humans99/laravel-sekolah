fa22c1f508b330a101fa7460abba9480ffbb6a29
Humans99 <samuelsidabalok12@gmail.com>
2025-06-06 10:03:13 +0700

    feat: add HasFactory trait and static factory property to Attendance model

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Attendance extends Model
 {
+    use HasFactory;
+    protected static $factory;
     protected $fillable = ['date', 'status', 'student_id', 'lesson_id'];
 
     public function student()
