4bfaefa9437469d7ea8677fccf5fd6559129b572
Humans99 <samuelsidabalok12@gmail.com>
2025-06-06 20:43:05 +0700

    fix: refactor StudentFactory to use a variable for username and hash password

--- database/factories/StudentFactory.php
+++ database/factories/StudentFactory.php
@@ -8,18 +8,21 @@ use App\Models\ParentModel;
 use App\Models\Student;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 
 class StudentFactory extends Factory
 {
     protected $model = Student::class;
     public function definition(): array
     {
+        $username = $this->faker->unique()->userName;
         return [
             'user_id' => User::factory()->create(['role' => 'student'])->id,
             'parent_id' => ParentModel::factory(),
             'grade_id' => Grade::factory(),
             'class_id' => ClassModel::factory(),
-            'username' => $this->faker->unique()->userName,
+            'username' => $username,
+            'password' => Hash::make($username),
             'name' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail(),
             'phone' => $this->faker->unique()->numerify("08##-####-####"),
