4711e7e7bd4ab847f886ed85eaa482d2b9f613d8
Humans99 <samuelsidabalok12@gmail.com>
2025-06-06 19:57:21 +0700

    feat: create ParentModelFactory for generating ParentModel instances

---
+++ database/factories/ParentModelFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\ParentModel;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ParentModelFactory extends Factory
+{
+    protected $model = ParentModel::class;
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory()->create(['role' => 'parent'])->id,
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+            'phone' => $this->faker->unique()->numerify("08##-####-####"),
+            'address' => $this->faker->address
+        ];
+    }
+}
