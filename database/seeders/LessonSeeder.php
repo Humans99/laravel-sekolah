532c0faf487c44a6a4537627da8963b2e562d4c0
Humans99 <samuelsidabalok12@gmail.com>
2025-06-07 10:36:01 +0700

    fix: refactor LessonSeeder to use dynamic subject and teacher selection

--- database/seeders/LessonSeeder.php
+++ database/seeders/LessonSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\ClassModel;
+use App\Models\Grade;
 use App\Models\Lesson;
 use App\Models\Subject;
 use App\Models\Teacher;
@@ -13,17 +14,33 @@ class LessonSeeder extends Seeder
 {
     public function run(): void
     {
-        $subjects = Subject::all();
-        $teachers = Teacher::all();
+        $faker = \Faker\Factory::create();
+        $grades = Grade::all();
         $classes = ClassModel::all();
 
-        foreach ($classes as $class) {
-            foreach ($subjects as $subject) {
-                Lesson::factory()->create([
-                    'class_id' => $class->id,
-                    'subject_id' => $subject->id,
-                    'teacher_id' => $teachers->random()->id,
-                ]);
+        foreach ($grades as $grade) {
+            foreach ($classes as $class) {
+                $subjects = Subject::inRandomOrder()->limit(4)->get();
+                foreach ($subjects as $subject) {
+                    $teacher = Teacher::where('subject_id', $subject->id)->inRandomOrder()->first();
+
+                    if (!$teacher) {
+                        continue;
+                    }
+
+                    $start = $faker->dateTimeBetween('08:00', '12:00');
+                    $end = (clone $start)->modify('+1 hour');
+
+                    Lesson::create([
+                        'name' => $grade->level . '-' . $class->name,
+                        'grade_id' => $grade->id,
+                        'class_id' => $class->id,
+                        'subject_id' => $teacher->subject->id,
+                        'teacher_id' => $teacher->id,
+                        'start' => $start,
+                        'end' => $end,
+                    ]);
+                }
             }
         }
     }
