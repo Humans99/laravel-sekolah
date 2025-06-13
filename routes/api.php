57b397da676aed22922f80eecd91d6f40d497c0a
Humans99 <samuelsidabalok12@gmail.com>
2025-06-13 16:17:19 +0700

    feat: add authentication routes for login, register, and logout

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\StudentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('students', StudentController::class);
+Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
+Route::post('logout', [AuthController::class, 'logout']);
