42e0b3284192369e1e7569fd695e744044f8080e
Humans99 <samuelsidabalok12@gmail.com>
2025-06-13 16:15:51 +0700

    feat: implement AuthController for user authentication and registration

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request)
+    {
+        $data = $request->validated();
+
+        if (!Auth::attempt($data)) {
+            return response()->json([
+                // 'message' => 'Invalid credentials',
+                'message' => 'Email or password is incorrect',
+                'errors' => true,
+            ], 401);
+        } else {
+            $user = Auth::user();
+            $token = $user->createToken('main')->plainTextToken;
+
+            return response()->json([
+                'message' => 'Login successful',
+                'user' => $user,
+                'token' => $token,
+            ], 200);
+        }
+    }
+
+    public function register(RegisterRequest $request)
+    {
+        $data = $request->validated();
+
+        $user = User::create([
+            'username' => $data['username'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response()->json([
+            'message' => 'User registered successfully',
+            'user' => $user,
+            'token' => $token,
+        ], 201);
+    }
+
+    public function logout(Request $request)
+    {
+        $user = $request->user();
+
+        $user->currentAccessToken()->delete();
+        return response()->json([
+            'message' => 'Logout successful',
+        ], 200);
+    }
+}
