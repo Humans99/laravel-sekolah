6f4e87efdebce64fc0d2cfbb7cc54c4adc9a5d9c
Humans99 <samuelsidabalok12@gmail.com>
2025-06-13 16:16:30 +0700

    feat: create RegisterRequest for user registration validation

---
+++ app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\Rules\Password;
+
+class RegisterRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'username' => 'required|unique:users|string|max:255',
+            'email' => 'required|email|unique:users,email|max:255',
+            'password' => [
+                'required',
+                Password::min(8)->letters()
+            ],
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        $errors = $validator->errors()->all();
+        $response = response()->json([
+            'message' => implode(' ', $errors),
+            'errors' => true
+        ], 422);
+
+        throw new HttpResponseException($response);
+    }
+}
