7c710582bcff6e9857a4c90e2b7e4ba1b2198a36
Humans99 <samuelsidabalok12@gmail.com>
2025-06-14 17:17:17 +0700

    fix: improve login error message and refactor login method for clarity

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
+use Hash;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -14,23 +15,22 @@ class AuthController extends Controller
     public function login(LoginRequest $request)
     {
         $data = $request->validated();
+        $loginType = filter_var($data['login'], FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
 
-        if (!Auth::attempt($data)) {
+        if (!Auth::attempt([$loginType => $data['login'], 'password' => $data['password']])) {
             return response()->json([
-                // 'message' => 'Invalid credentials',
-                'message' => 'Email or password is incorrect',
+                'message' => 'Email/Username or password is incorrect',
                 'errors' => true,
             ], 401);
-        } else {
-            $user = Auth::user();
-            $token = $user->createToken('main')->plainTextToken;
-
-            return response()->json([
-                'message' => 'Login successful',
-                'user' => $user,
-                'token' => $token,
-            ], 200);
         }
+
+        $user = Auth::user();
+        $token = $user->createToken('main')->plainTextToken;
+        return response()->json([
+            'message' => 'Login successful',
+            'user' => $user,
+            'token' => $token,
+        ], 200);
     }
 
     public function register(RegisterRequest $request)
