3f01da177445f9845b1637460e1cbd17d822d04e
Humans99 <samuelsidabalok12@gmail.com>
2025-06-16 21:59:52 +0700

    fix: add unique code generation to TeacherFactory and update phone number format

--- database/factories/TeacherFactory.php
+++ database/factories/TeacherFactory.php
@@ -6,6 +6,7 @@ use App\Models\Subject;
 use App\Models\Teacher;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Str;
 
 class TeacherFactory extends Factory
 {
@@ -16,11 +17,20 @@ class TeacherFactory extends Factory
         return [
             'user_id' => User::factory(),
             'subject_id' => Subject::inRandomOrder()->first()->id,
+            'code' => $this->generateUniqueCode(),
             'name' => $this->faker->name,
-            'phone' => $this->faker->unique()->numerify("08##-####-####"),
+            'phone' => $this->faker->unique()->numerify("08##########"),
             'address' => $this->faker->address,
             'bloodType' => $this->faker->randomElement(['A', "B", 'AB', 'O']),
             'gender' => $this->faker->randomElement(['Pria', "Wanita"]),
         ];
+    }
+
+    private function generateUniqueCode(): string
+    {
+        do {
+            $code = $this->faker->regexify('[A-Z]{3}');
+        } while (Teacher::where('code', $code)->exists());
+        return $code;
     }
 }
