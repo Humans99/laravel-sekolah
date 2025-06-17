82f71bab42f9d88e5c38f606fa67209d32de2a47
Humans99 <samuelsidabalok12@gmail.com>
2025-06-17 23:27:59 +0700

    fix: remove unused StudentController and related imports from api routes

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\Api\StudentController;
 use App\Http\Controllers\Api\TeacherController;
-use App\Models\Student;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('login', [AuthController::class, 'login']);
