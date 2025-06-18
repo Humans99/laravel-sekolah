e135bd87e625904293c98fd2b17112d1377d7c02
Humans99 <samuelsidabalok12@gmail.com>
2025-06-18 16:07:57 +0700

    feat: enhance StudentCollection response format with status and message

--- app/Http/Resources/StudentCollection.php
+++ app/Http/Resources/StudentCollection.php
@@ -4,11 +4,16 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\ResourceCollection;
+use Symfony\Component\HttpFoundation\Response;
 
 class StudentCollection extends ResourceCollection
 {
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'status' => Response::HTTP_OK,
+            'message' => "Student retrieved successfully",
+            'data' => StudentResource::collection($this->collection),
+        ];
     }
 }
