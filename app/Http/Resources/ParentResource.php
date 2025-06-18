df8801df8bbf0f362f005178cb4762ea1bdc342f
Humans99 <samuelsidabalok12@gmail.com>
2025-06-18 16:12:53 +0700

    refactor: customize ParentResource to return specific parent attributes

--- app/Http/Resources/ParentResource.php
+++ app/Http/Resources/ParentResource.php
@@ -7,13 +7,13 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class ParentResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'phone' => $this->phone,
+            'address' => $this->address,
+        ];
     }
 }
